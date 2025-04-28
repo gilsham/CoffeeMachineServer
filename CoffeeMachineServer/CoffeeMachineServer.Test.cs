93d64b1fed8decec016d245d4c3b500d93bf9b70
Ben <yougotben@gmail.com>
2025-04-28 15:38:40 +1200

    add tests

---
+++ CoffeeMachineServer/CoffeeMachineServer.Test.cs
@@ -0,0 +1,28 @@
+﻿using CoffeeMachineServer.Interfaces;
+using Xunit;
+using FluentAssertions;
+
+namespace CoffeeMachineServer;
+
+public class CoffeeMachineServerTest
+{
+    [Fact]
+    public void TryBrewCoffee()
+    {
+        var testMachine = new CoffeeMachineInterface();
+
+        testMachine.TryBrewCoffee().Should().Be(CoffeeMachineInterface.Status.Brewing);
+    }
+
+    [Fact]
+    public void TryBrewCoffee_FiveTimes()
+    {
+        var testMachine = new CoffeeMachineInterface();
+
+        testMachine.TryBrewCoffee().Should().Be(CoffeeMachineInterface.Status.Brewing);
+        testMachine.TryBrewCoffee().Should().Be(CoffeeMachineInterface.Status.Brewing);
+        testMachine.TryBrewCoffee().Should().Be(CoffeeMachineInterface.Status.Brewing);
+        testMachine.TryBrewCoffee().Should().Be(CoffeeMachineInterface.Status.Brewing);
+        testMachine.TryBrewCoffee().Should().Be(CoffeeMachineInterface.Status.Empty);
+    }
+}
